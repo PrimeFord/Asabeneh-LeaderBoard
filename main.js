54428f2870f8057a606f4d5cc79d9d1a3c1f9439
PrimeFord <bashfordbash@gmail.com>
2023-01-01 01:42:05 +0100

    Day28

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ const country = document.querySelector(".country");
 const score = document.querySelector(".score");
 const add = document.querySelector(".add");
 const req = document.querySelector(".req");
+const contents = document.querySelector(".contents");
 const names = document.querySelector(".names");
 const time = document.querySelector(".time");
 const ctry = document.querySelector(".ctry");
 const scr = document.querySelector(".scr");
+const action = document.querySelector(".action");
 const del = document.querySelector(".del");
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
@@ -57,18 +59,43 @@ add.addEventListener("click", () => {
     country.value !== "" &&
     score.value !== ""
   ) {
-    names.textContent =
-      firstname.value.toLocaleUpperCase() +
-      " " +
-      lastname.value.toLocaleUpperCase();
-    time.textContent = timeNow;
-    ctry.textContent = country.value.toLocaleUpperCase();
-    scr.textContent = score.value;
+    req.textContent = "";
+    contents.innerHTML += `<div class="one">
+    <div class="name">
+      <p class="names">${
+        firstname.value.toLocaleUpperCase() +
+        " " +
+        lastname.value.toLocaleUpperCase()
+      }</p>
+      <p class="time">${timeNow}</p>
+    </div>
+    <p class="ctry">${country.value.toLocaleUpperCase()}</p>
+    <p class="scr">${score.value}</p>
+    <div class="action">
+      <div class="del">
+        <img src="./media/delete-bin-6-line.png" alt="delete" />
+      </div>
+      <div class="plus">+5</div>
+      <div class="minus">-5</div>
+    </div>
+  </div>`;
   }
 });
-// plus.addEventListener("click", () => {
-//   parseInt(score.value) + 5;
-// });
+plus.addEventListener("click", () => {
+  let sccr = parseInt(scr.textContent);
+  sccr += 5;
+  scr.textContent = sccr;
+});
+
+// req.textContent = "";
+// names.textContent =
+//   firstname.value.toLocaleUpperCase() +
+//   " " +
+//   lastname.value.toLocaleUpperCase();
+// time.textContent = timeNow;
+// ctry.textContent = country.value.toLocaleUpperCase();
+// scr.textContent = score.value;
+
 // minus.addEventListener("click", () => {
 //   parseInt(score.value) - 5;
 // });
